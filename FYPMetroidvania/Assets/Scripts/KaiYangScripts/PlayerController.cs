b1127e9aa6037c49c60e21a89b2e4b60ad0d31bb
HiImKnurd <hiimknurd42wenty@gmail.com>
2025-09-16 10:09:30 +0800

    updating merge

--- FYPMetroidvania/Assets/Scripts/KaiYangScripts/PlayerController.cs
+++ FYPMetroidvania/Assets/Scripts/KaiYangScripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController instance;
+
     [Header("References")]
     public Transform spriteTransform;
 
@@ -25,10 +27,10 @@ public class PlayerController : MonoBehaviour
 
 
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
     private Animator animator;
     private SpriteRenderer spriteRenderer;
-    private Vector2 moveInput;
+    public Vector2 moveInput;
     private Vector2 dashDirection;
     private bool isGrounded;
     private bool isDashing;
@@ -45,6 +47,7 @@ public class PlayerController : MonoBehaviour
 
     private void Awake()
     {
+        instance = this;
         rb = GetComponent<Rigidbody2D>();
 
         //  Auto-find Animator & SpriteRenderer
@@ -136,7 +139,7 @@ public class PlayerController : MonoBehaviour
 
 
 
-    private void Flip()
+    public void Flip()
     {
         facingRight = !facingRight;
 
