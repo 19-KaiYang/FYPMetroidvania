202175452928cf1e1fb317703014bac3d07a6461
KaiYang <114893002+19-KaiYang@users.noreply.github.com>
2025-10-07 17:22:27 +0800

    FIXED FLEET OF FOOT

--- FYPMetroidvania/Assets/Scripts/Upgrade System/Upgrade Scripts/FleetOfFootUpgrade.cs
+++ FYPMetroidvania/Assets/Scripts/Upgrade System/Upgrade Scripts/FleetOfFootUpgrade.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Fleet of Foot", menuName = "Upgrades/Mobility/Fleet of Foot", order = 1)]
 public class FleetOfFootUpgrade : Upgrade
 {
-    public float dashSpeedMultiplier = 1.25f; 
-    public int extraDashes = 1;               
-
     public override void OnApply(UpgradeManager upgradeManager)
     {
-        PlayerController.instance.dashSpeed *= dashSpeedMultiplier;
-        PlayerController.instance.dashCount += extraDashes;
+        PlayerController.instance.dashSpeed *= 1.25f;
+        PlayerController.instance.dashCount = 2;
+        PlayerController.instance.dashesRemaining = 2;
+
+        Debug.Log("[UPGRADE] Fleet of Foot applied. dashCount=2, dashesRemaining=2, dashSpeed=" + PlayerController.instance.dashSpeed);
     }
 
     public override void OnRemove(UpgradeManager upgradeManager)
     {
-        PlayerController.instance.dashSpeed /= dashSpeedMultiplier;
-        PlayerController.instance.dashCount -= extraDashes;
+        PlayerController.instance.dashSpeed /= 1.25f;
+        PlayerController.instance.dashCount = 1;
+        PlayerController.instance.dashesRemaining = 1;
+
+        Debug.Log("[UPGRADE] Fleet of Foot removed. dashCount=1, dashesRemaining=1, dashSpeed=" + PlayerController.instance.dashSpeed);
     }
+
+
 }
