cf6058dbf6eb60e3aa61b7234f9f899a20f4fc58
gniqoaixgnahz <234266y@mymail.nyp.edu.sg>
2025-11-10 09:51:41 +0800

    boss

--- FYPMetroidvania/Assets/Zhang/Script/Enemy/EnemyAnimationEvent.cs
+++ FYPMetroidvania/Assets/Zhang/Script/Enemy/EnemyAnimationEvent.cs
@@ -1,14 +1,17 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class EnemyAnimationEvent : MonoBehaviour
 {
     private MeleeEnemy brawler;
     private Spearman spearman;
+    private TruckBoss boss;
 
     private void Awake()
     {
         brawler = GetComponentInParent<MeleeEnemy>();
         spearman = GetComponentInParent<Spearman>();
+        boss = GetComponentInParent<TruckBoss>();
     }
 
     private void BrawlerClawFinished()
@@ -27,5 +30,39 @@ public class EnemyAnimationEvent : MonoBehaviour
     private void SpearManThrustFinished()
     {
         spearman.isThrustFinished = true;
+    }
+
+    private void TruckMoveTrue()
+    {
+        boss.canMove = true;
+    }
+    private void TruckMoveFalse()
+    {
+        boss.canMove = false;
+    }
+
+    private void BurstFinished()
+    {
+        boss.bFinished = true;
+    }
+
+    private void SlashFinished()
+    {
+        boss.slashFinished = true;
+    }
+
+    private void StartSlash()
+    {
+        boss.startSlash = true;
+    }
+
+    private void ChangePhase()
+    {
+        boss.changePhase = true;
+    }
+
+    private void StartRevving()
+    {
+        boss.startRevving = true;
     }
 }
--- FYPMetroidvania/Assets/Zhang/Script/Enemy/TruckBoss/Ramp.cs
+++ FYPMetroidvania/Assets/Zhang/Script/Enemy/TruckBoss/Ramp.cs
@@ -2,15 +2,50 @@ using UnityEngine;
 
 public class Ramp : MonoBehaviour
 {
-    
+    public CrowdControlState currentCCState = CrowdControlState.None;
+    private PlayerController player;
+    private TruckBoss owner;
+    [SerializeField] private float ownerAttackDamage;
+    [SerializeField] private float attackMultiplier;
+    [SerializeField] private float finalDamage;
+
+    public void Init(float _attackDamage, TruckBoss _boss)
+    {
+        ownerAttackDamage = _attackDamage;
+        owner = _boss;
+    }
+    public void SetOwner(TruckBoss _boss)
+    {
+        owner = _boss;
+    }
+    void Awake()
+    {
+        player = FindFirstObjectByType<PlayerController>();
+    }
+
     void Start()
     {
-        
+        finalDamage = attackMultiplier * owner.attackDamage;
     }
 
     
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Health p = collision.GetComponent<Health>();
+            Vector2 dir;
+            dir = (collision.transform.position - this.transform.position).normalized;
+
+            p.TakeDamage(finalDamage, dir, true, CrowdControlState.Knockdown, 0f);
+
+            if (currentCCState == CrowdControlState.Stunned) p.ApplyStun(1, dir);
+            else if (currentCCState == CrowdControlState.Knockdown) p.ApplyKnockdown(1, false, dir);
+        }
     }
 }
