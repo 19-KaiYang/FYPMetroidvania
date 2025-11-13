80bc021982116cf0b5a0e61ee919176b6d0f566f
HiImKnurd <hiimknurd42wenty@gmail.com>
2025-11-13 13:51:21 +0800

    fixing cutscene skip and boss room decor

--- FYPMetroidvania/Assets/Scripts/Dialogue/CutsceneHandler.cs
+++ FYPMetroidvania/Assets/Scripts/Dialogue/CutsceneHandler.cs
@@ -5,15 +5,26 @@ public class CutsceneHandler : MonoBehaviour
 {
     public bool inCutscene;
     [SerializeField] PlayableDirector cutscene;
+    [SerializeField] Canvas skipIndicator;
+    public bool canSkip;
     private void Start()
     {
-        inCutscene = true;
+        //inCutscene = true;
+        skipIndicator.enabled = false;
+        canSkip = false;
     }
     // Update is called once per frame
     void Update()
     {
+        if (cutscene.state == PlayState.Paused) inCutscene = true;
+        else
+        {
+            inCutscene = false;
+            skipIndicator.enabled = false;
+            canSkip = false;
+        }
         if (!inCutscene) return;
-        if (Input.GetKeyDown(KeyCode.Backspace))
+        if (Input.GetKeyDown(KeyCode.Backspace) && canSkip)
         {
             //cutscene.Resume();
             inCutscene = false;
@@ -22,5 +33,10 @@ public class CutsceneHandler : MonoBehaviour
             cutscene.Evaluate();
             if (!AudioManager.instance.isPlayingBGM) AudioManager.instance.PlayBGM(BGMType.OPENING_CUTSCENE);
         }
+    }
+    public void CanSkip()
+    {
+        skipIndicator.enabled = true;
+        canSkip = true;
     }
 }
