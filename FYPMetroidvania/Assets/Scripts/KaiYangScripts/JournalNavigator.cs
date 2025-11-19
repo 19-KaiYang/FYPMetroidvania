368476d2a1d288b9d4a17614f826bb9e4b77aafc
KaiYang <114893002+19-KaiYang@users.noreply.github.com>
2025-11-19 13:55:23 +0800

    Added BUTTONS for journal

---
+++ FYPMetroidvania/Assets/Scripts/KaiYangScripts/JournalNavigator.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalNavigator : MonoBehaviour
+{
+    [Header("Page GameObjects")]
+    public GameObject[] pages; 
+
+    [Header("Navigation Buttons")]
+    public Button nextButton;
+    public Button previousButton;
+
+    private int currentPageIndex = 0;
+
+    void Start()
+    {
+        nextButton.onClick.AddListener(NextPage);
+        previousButton.onClick.AddListener(PreviousPage);
+        ShowPage(currentPageIndex);
+    }
+
+    void ShowPage(int index)
+    {
+        // Disable all pages
+        for (int i = 0; i < pages.Length; i++)
+        {
+            pages[i].SetActive(false);
+        }
+        if (index >= 0 && index < pages.Length)
+        {
+            pages[index].SetActive(true);
+        }
+        UpdateButtons();
+    }
+
+    void NextPage()
+    {
+        if (currentPageIndex < pages.Length - 1)
+        {
+            currentPageIndex++;
+            ShowPage(currentPageIndex);
+        }
+    }
+
+    void PreviousPage()
+    {
+        if (currentPageIndex > 0)
+        {
+            currentPageIndex--;
+            ShowPage(currentPageIndex);
+        }
+    }
+
+    void UpdateButtons()
+    {
+        previousButton.gameObject.SetActive(currentPageIndex > 0);
+        nextButton.gameObject.SetActive(currentPageIndex < pages.Length - 1);
+    }
+}
